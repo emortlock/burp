ef303b629deedb8da03feee29b04cbfcbbfa4e4c
Edward Mortlock <e.mortlock@gmail.com>
2018-08-05 19:46:00 +0100

    Added bundle-analysis

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const withCSS = require('@zeit/next-css')
 
 const flags = require('./build/webpack/flags')
@@ -5,23 +6,27 @@ const polyfills = require('./build/webpack/polyfills')
 const loaders = require('./build/webpack/loaders')
 const plugins = require('./build/webpack/plugins')
 
-module.exports = withCSS({
-  publicRuntimeConfig: flags,
-  webpack: (config, meta) => {
-    // Fix to allow winston logging client side
-    const node = !meta.isServer
-      ? {
-          node: { fs: 'empty' },
-        }
-      : {}
+module.exports = withBundleAnalyzer(
+  withCSS({
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    publicRuntimeConfig: flags,
+    webpack: (config, meta) => {
+      // Fix to allow winston logging client side
+      const node = !meta.isServer
+        ? {
+            node: { fs: 'empty' },
+          }
+        : {}
 
-    return Object.assign(
-      {},
-      config,
-      polyfills(config, meta),
-      plugins(config, meta),
-      loaders(config, meta),
-      node,
-    )
-  },
-})
+      return Object.assign(
+        {},
+        config,
+        polyfills(config, meta),
+        plugins(config, meta),
+        loaders(config, meta),
+        node,
+      )
+    },
+  }),
+)
