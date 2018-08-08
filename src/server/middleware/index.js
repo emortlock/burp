723a3514515ad13b0bf4b5734f8caa088aea5711
Ed Mortlock <emortlock@users.noreply.github.com>
2018-08-08 19:58:30 +0100

    Add docker & nginx reverse proxy (#13)
    nginx reverse proxy provides:
    
    * http2
    * gzip compression
    * url redirects / rewrites

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -1,20 +1,14 @@
 const bodyParser = require('body-parser')
-const compression = require('compression')
 const helmet = require('helmet')
-
-const config = require('../config')
 
 const health = require('./health')
 const metrics = require('./metrics')
-const redirectURL = require('./redirectURL')
 const validatePost = require('./validatePost')
 
 module.exports = [
   health,
   metrics,
   helmet(),
-  redirectURL(),
-  !config.isDev && compression(),
   validatePost,
   bodyParser.json({ limit: '20mb' }),
   bodyParser.urlencoded({ limit: '20mb', extended: false }),
